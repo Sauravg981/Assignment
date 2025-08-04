b2a3df6dcb595e86fbaee6347f3ffdcdff78ecae
Saurav <86883925+Sauravg981@users.noreply.github.com>
2025-08-04 23:01:54 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+#Task 1: Perform Basic Mathematical Operations
+a= int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+
---
+++ task2.py
@@ -0,0 +1,5 @@
+#Task 2: Create a Personalized Greeting
+a = str(input("Enter your first name: "))
+b= str(input("Enter your second name: "))
+
+print("hey "+ a +" " +b+"! Welcome to Python program")
